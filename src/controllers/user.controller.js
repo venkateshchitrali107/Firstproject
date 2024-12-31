ae85b97469caf4226e1d7563aaef59db29a80874
venkatesh <venkateshchitrali107@gmail.com>
2024-12-31 22:48:58 +0530

    made the first api hit

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,34 @@
 import { asyncHandlerAwait } from '../utils/asyncHandler.js'
+import { ApiError } from '../utils/ApiError.js'
+import { User } from '../models/user.model.js'
+import { ApiResponse } from '../utils/ApiResponse.js'
 
 const registerUser = asyncHandlerAwait(async (req, res) => {
-    res.status(200).json({
-        message: "ok"
-    })
+    const { username, email, password, fullname } = req.body
+
+    if ([username, email, password, fullname].some((field) => field?.trim() === "")) {
+        throw new ApiError(400, "All fields are required");
+    }
+
+    const existedUser = await User.findOne({ $or: [{ username }, { email }] })
+    if (existedUser) {
+        throw new ApiError(400, "User already exists");
+    }
+
+    const user = new User({ username, email, password, fullname })
+    await user.save()
+
+    const createUser = User.findById(user._id).select("-password -refreshToken")
+    if (createUser) {
+        res
+            .status(201)
+            .json(new ApiResponse(201,
+                "User registered successfully",
+            ))
+    } else {
+        throw new ApiError(500, "User registration failed")
+    }
+
 })
 
 
--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -16,9 +16,6 @@ class ApiError extends Error {
             Error.captureStackTrace(this, this.constructor)
         }
     }
-
-
-
 }
 
 export { ApiError }
